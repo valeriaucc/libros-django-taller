6bea5f470f611a9958ec6b793d183d949f3428a2
Valeria Torres <valeria.gongora@campusucc.edu.co>
2026-04-23 08:40:38 -0500

    subiendo views genericas list y delete

--- proyecto/gestion/views.py
+++ proyecto/gestion/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Autor, Libro
 from .forms import AutorForm, LibroForm
 
@@ -34,3 +36,18 @@ def eliminar_autor(request, pk):
         autor.delete()
         return redirect('lista_autores')
     return render(request, 'gestion/autor_confirm_delete.html', {'autor': autor})
+
+# LIST
+class AutorListView(ListView):
+    model = Autor
+    template_name = 'gestion/lista_autores.html'
+    context_object_name = 'autores'
+
+
+# CREATE
+class AutorCreateView(CreateView):
+    model = Autor
+    form_class = AutorForm
+    template_name = 'gestion/autor_form.html'
+    success_url = reverse_lazy('lista_autores')
+
