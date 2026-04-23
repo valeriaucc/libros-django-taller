b4079a91864573c6a6d70ae6df623d9095e198d6
Valeria Torres <valeria.gongora@campusucc.edu.co>
2026-04-23 09:25:27 -0500

    subo

--- proyecto/gestion/forms.py
+++ proyecto/gestion/forms.py
@@ -4,9 +4,9 @@ from .models import Autor, Libro
 class AutorForm(forms.ModelForm):
     class Meta:
         model = Autor
-        fields = ['nombre', 'correo', 'nacionalidad', 'fecha_nacimiento', 'biografia']
+        fields = '__all__'
 
 class LibroForm(forms.ModelForm):
     class Meta:
         model = Libro
-    fields = ['titulo', 'fecha_publicacion', 'genero', 'isbn', 'autor']
+        fields = '__all__'
