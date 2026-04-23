a5731f4155c36dcf79d40f123cefb13b5f553fb8
Gabonarvaezm <160960360+Gabonarvaezm@users.noreply.github.com>
2026-04-23 08:13:10 -0500

    subimos modelo Libro

--- proyecto/gestion/models.py
+++ proyecto/gestion/models.py
@@ -10,3 +10,13 @@ class Autor(models.Model):
     def __str__(self):
         return self.nombre
     
+class Libro(models.Model):
+    titulo = models.CharField(max_length=150)
+    fecha_publicacion = models.DateField()
+    genero = models.CharField(max_length=50)
+    isbn = models.CharField(max_length=20, unique=True)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, related_name='libros')
+
+    def __str__(self):
+        return self.titulo
+    
