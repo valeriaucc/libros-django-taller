13b691de773a42cac1f069982f3893b934bc4f89
Valeria Torres <valeria.gongora@campusucc.edu.co>
2026-04-23 09:15:00 -0500

    subiendo admin

--- proyecto/gestion/admin.py
+++ proyecto/gestion/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-
-from proyecto.gestion.models import Autor, Libro
+from .models import Autor, Libro
 
 @admin.register(Autor)
 class AutorAdmin(admin.ModelAdmin):
- list_display = ('nombre', 'correo', 'nacionalidad', 'fecha_nacimiento')
+    list_display = ('nombre', 'correo', 'nacionalidad', 'fecha_nacimiento')
+
 @admin.register(Libro)
 class LibroAdmin(admin.ModelAdmin):
- list_display = ('titulo', 'genero', 'fecha_publicacion', 'autor', 'isbn')
- list_filter = ('autor', 'genero', 'fecha_publicacion')
+    list_display = ('titulo', 'genero', 'fecha_publicacion', 'autor', 'isbn')
+    list_filter = ('autor', 'genero', 'fecha_publicacion')
