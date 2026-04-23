3f8cd2a159370b2747dc17e2df55a6347faca11c
vanessaucc <julieth.mena@campusucc.edu.co>
2026-04-23 07:58:30 -0500

    modelo autor

--- proyecto/gestion/models.py
+++ proyecto/gestion/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField(unique=True)
+    nacionalidad = models.CharField(max_length=50)
+    fecha_nacimiento = models.DateField()
+    biografia = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+    
