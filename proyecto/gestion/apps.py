2f39c6c76073ce2f7b68aef281e523d269d95cd9
Valeria Torres <valeria.gongora@campusucc.edu.co>
2026-04-23 10:56:02 -0500

    Avoid Vercel admin login write on SQLite

--- proyecto/gestion/apps.py
+++ proyecto/gestion/apps.py
@@ -3,3 +3,13 @@ from django.apps import AppConfig
 
 class GestionConfig(AppConfig):
     name = 'gestion'
+
+    def ready(self):
+        if not __import__('os').getenv('VERCEL'):
+            return
+
+        from django.contrib.auth import get_user_model
+        from django.contrib.auth.models import update_last_login
+        from django.contrib.auth.signals import user_logged_in
+
+        user_logged_in.disconnect(update_last_login, sender=get_user_model())
